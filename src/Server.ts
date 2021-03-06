a931e68dea59b29ea4ff91fa9583490827382fed
Guilherme Hideki Noce <hidekinoce@gmail.com>
2021-03-06 01:07:17 -0300

    deploy

--- src/Server.ts
+++ src/Server.ts
@@ -7,7 +7,13 @@ import routes from './Routes/index';
 //app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-app.listen(process.env.PORT || 3333, () => {
-  console.log('🚀 Server started on port 3333!')
+const porta = process.env.PORT
+app.listen(porta || 3333, () => {
+  if (porta != '3333'){
+    console.log('🚀 Server started on port '+porta+'!')
+  }else{
+    console.log('🚀 Server started on port 3333!')
+  }
+  
+  
 });
